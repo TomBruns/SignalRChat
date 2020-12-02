5e859414e5c5045ea700b946dc34ac6361287686
Tom Bruns <xtobr39@hotmail.com>
2020-12-02 15:26:13 -0500

    Add project files.

---
+++ SignalRChatWeb/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalRChatWeb.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHubContext<ChatHub> _messageHub;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHubContext<ChatHub> messageHub)
+        {
+            _logger = logger;
+            _messageHub = messageHub;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public void SendMsg(string message)
+        {
+            _messageHub.Clients.All.SendAsync("ReceiveMessage", "Server", message);
+        }
+    }
+}
+
